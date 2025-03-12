56ac6fe1469b71ab12aea0345726587999d09e2b
David Reese <eseer.divad@gmail.com>
2025-03-12 04:51:29 -0400

    Delete JS frontend and switch to django template

--- valuable_gathering/valuable_gathering/views.py
+++ valuable_gathering/valuable_gathering/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.conf import settings
 import os
+import csv
 from . import timed_nodes
 
 def index(request):
@@ -12,9 +13,18 @@ def index(request):
     sorted_file = os.path.join(base_dir, "final_nodes_with_ids_sorted.csv")
     market_file = os.path.join(base_dir, "final_nodes_with_ids_market.csv")
 
+    # Run functions in sequence
     timed_nodes.clean_unspoiled_data(input_file, cleaned_file)
     timed_nodes.assign_ids(cleaned_file, final_file)
     timed_nodes.sort_for_current_time(final_file, sorted_file)
     timed_nodes.generate_market_data(sorted_file, market_file)
-
-    return HttpResponse("All functions executed in sequence. Check the generated output files.")
+    
+    # Read the market CSV into a list of dictionaries
+    table_data = []
+    with open(market_file, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            table_data.append(row)
+    
+    # Render the data in the template
+    return render(request, "index.html", {"table_data": table_data})
