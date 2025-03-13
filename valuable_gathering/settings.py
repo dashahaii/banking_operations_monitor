1c3ec3402149d8a61ecf1de1b88a94bdf686e74b
David Reese <eseer.divad@gmail.com>
2025-03-13 12:58:56 -0400

    Update allowed hosts

--- valuable_gathering/settings.py
+++ valuable_gathering/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-77leiz20@_s*v$f#wuv5th5h32$u)1m&84(6z+xz)$gpmvvcpq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['100.98.30.53', 'localhost', '127.0.0.1']
 
 
 # Application definition
